7b6a31ad9a9c217bb59586403a6e28d8dbfbb4a8
Student <b2051305@debian>
2021-10-09 18:37:22 +0300

    delete aisd2021

--- aisd2021/lab4b/func.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNC
-#define FUNC
-#include <stdio.h>
-int get_int(int *num);
-int get_u_int(unsigned int *num);
-int get_float(float *num);
-char *get_str();
-int dialog (const char *msgs[], int n);
-#endif
--- aisd2021/lab5/func.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNC
-#define FUNC
-#include <stdio.h>
-int get_int(int *num);
-char *get_str();
-int dialog (const char *msgs[], int n);
-#endif
